610ae6d761ab8a691324aa5287350335338960b6
yassineafaila <yassineafaila55@gmail.com>
2024-07-05 13:11:16 +0100

    feat:Add the logique for handling login request

--- backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
@@ -9,12 +10,32 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\Auth\LoginRequest;
 
-class AuthenticatedSessioLogiController extends Controller
+class AuthenticatedSessionController extends Controller
 {
     /**
      * Handle an incoming authentication request.
      */
-   
+    public function store(LoginRequest $request): JsonResponse
+    {
+        // Attempt to authenticate the user
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid Credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user   = User::where('email', $request->email)->firstOrFail();
+        $token  = $user->createToken('auth_token')->plainTextToken;
+
+        $user->token = $token;
+        // Return the token in the response
+        return response()->json([
+            'message'       => 'Login success',
+            'access_token'  => $token,
+            'token_type'    => 'Bearer'
+        ], Response::HTTP_OK);
+    }
 
     /**
      * Destroy an authenticated session.
