838df722ac1cd45634b6a744377a48df0ee4b7a2
yassineafaila <yassineafaila55@gmail.com>
2024-07-05 13:12:36 +0100

    feat:Add the logique for handling register request

--- backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -19,15 +20,22 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(RegisterRequest $request): Response
+    public function store(RegisterRequest $request): JsonResponse
     {
-
+        // Create a new user
         $user = User::create([
-            'username' => $request->name,
+            'username' => $request->username,
             'email' => $request->email,
-            'password' => Hash::make($request->string('password')),
+            'password' => Hash::make($request->password),
         ]);
 
-        return response()->noContent();
+        // Create a new token
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        // Return the token in the response
+        return response()->json([
+            'status' => 'success',
+            'token' => $token,
+        ], Response::HTTP_CREATED);
     }
 }
