be37ae07be3456bc8cfd5cd6a6c1bcebf8765e17
yassineafaila <yassineafaila55@gmail.com>
2024-07-05 13:15:54 +0100

    add the relationship between the User and The Event Model

--- backend/app/Models/Event.php
+++ backend/app/Models/Event.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Event extends Model
 {
     use HasFactory;
+    protected $table = 'events';
     protected $fillable = [
         'name',
         'description',
@@ -17,4 +20,31 @@ class Event extends Model
         'end_time',
         'organizer_id',
     ];
+    protected $dates = [
+        'start_time', 'end_time', // Specify date attributes
+    ];
+
+    /**
+     * Get the user who organized this event.
+     */
+    public function organizer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'organizer_id');
+    }
+
+    /**
+     * Get the speakers for this event.
+     */
+    public function speakers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'event_speakers');
+    }
+
+    /**
+     * Get the attendees for this event.
+     */
+    public function attendees(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'event_attendees');
+    }
 }
