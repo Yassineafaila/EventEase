64179a7cd93a181000d32cd992ab09edbaf23ac1
rachidcooder <cooderherou@gmail.com>
2024-07-10 21:50:18 +0100

    add to test

--- frontend/src/components/common/ui/Footer.tsx
+++ frontend/src/components/common/ui/Footer.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Footer() {
   return (
     <div className='container mx-auto max-w-[1280px]'>
-      Footer
+        <div className='border-t-2 border-gray-300 flex flex:col md:flex-row md:justify-between py-5 text-sm'>
+      <div className='mb-4'>
+        <a href='#' className='mx-2.5'>About</a>
+        <a href='#' className='mx-2.5'>Privacy Policy</a>
+        <a href='#' className='mx-2.5'>Terms of Services</a>
+      </div>
+    <p>© Copyright Reserved 2023</p>
+    </div>
 
     </div>
   )
--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import NavBar from '../components/common/ui/NavBar'
 import Footer from '../components/common/ui/Footer'
+
+
 interface IPropsLayout {
     children:React.ReactNode
 }
+
+
 function MainLayout({children}:IPropsLayout) {
   return (
       <>
