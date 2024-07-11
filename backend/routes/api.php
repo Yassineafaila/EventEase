6afe02fafce3019d2b4d53173d1d36c7ad709d66
yassineafaila <yassineafaila55@gmail.com>
2024-07-11 16:00:46 +0100

    modified

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\EventController;
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
